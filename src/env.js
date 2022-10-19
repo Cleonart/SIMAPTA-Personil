30098540b157207d0089dfa743693f08fdcbed83
zredhard <cleonartd@gmail.com>
2022-10-19 08:47:14 +0800

    [PATCH] Update Personil

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
 export default {
-  BASE_URL: "http://localhost:8000",
-  // BASE_URL: "https://api.simapta.web.id",
+  // BASE_URL: "http://localhost:8000",
+  BASE_URL: "https://api.simapta.web.id",
 };
